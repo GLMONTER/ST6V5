8f638bdef1befb57f0952a55080d25f0b07a0baf
Jed Heath <JHeath@mhs.usd266.com>
2019-03-20 15:23:32 -0500

    add all again

---
+++ .vscode/cquery_cached_index/c@@users@jheath@documents@st6v5/src@opcontrol.cpp
@@ -0,0 +1,221 @@
+#include "main.h"
+/**
+* Runs the operator control code. This function will be started in its own task
+* with the default priority and stack size whenever the robot is enabled via
+* the Field Management System or the VEX Competition Switch in the operator
+* control mode.
+*
+* If no competition control is connected, this function will run immediately
+* following initialize().
+*
+* If the robot is disabled or communications is lost, the
+* operator control task will be stopped. Re-enabling the robot will restart the
+* task, not resume it from where it left off.
+*/
+
+//toggle and press bool for forward motion of drum
+int buttonToggleF = 0;
+int buttonPressedF = 0;
+
+//toggle and press bool for backward motion of drum
+int buttonToggleR = 0;
+int buttonPressedR = 0;
+
+//the toggle and press variables for the fly wheel
+int flyToggle = 0;
+int flyPressed = 0;
+
+//the reverse drive variables for toggle and press
+int reverseToggle = 0;
+int reversePressed = 0;
+
+//left drive train, both not reversed
+pros::Motor leftMotB(1, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor leftMotF(2, pros::E_MOTOR_GEARSET_18, false);
+
+//the right drive train, both reversed
+pros::Motor rightMotB(11, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor rightMotF(12, pros::E_MOTOR_GEARSET_18, true);
+
+//the fly wheel, with the high speed gearset, one reversed one not.
+pros::Motor fly(20, pros::E_MOTOR_GEARSET_06, true);
+pros::Motor fly2(10, pros::E_MOTOR_GEARSET_06, false);
+
+//both of the loaders, one reversed one not.
+pros::Motor LoadServ(19, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor LoadServ2(9, pros::E_MOTOR_GEARSET_18, false);
+
+//the vision sensor, telling the sensor to put the origin in the center, (0,0).
+pros::Vision vSensor(8, pros::E_VISION_ZERO_CENTER);
+
+//the main controller variable.
+pros::Controller mController(pros::E_CONTROLLER_MASTER);
+
+//a function that polls the controls for the fly wheel the acts on the fly wheel based on the controls.
+void pollTFly()
+{
+		//the toggle for the fly wheel.
+		if(mController.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+		{
+				if(!flyPressed)
+				{
+						flyToggle = 1 - flyToggle;
+
+						flyPressed = 1;
+				}
+		}
+		else
+			flyPressed = 0;
+
+		//if the fly wheel toggle is true, set the shooter to 600 RPM, else turn it off
+		if(flyToggle)
+		{
+				fly.move_velocity(600);
+        fly2.move_velocity(600);
+		}
+		else
+    {
+  			if(!flyToggle)
+  			{
+  					fly.move(0);
+            fly2.move(0);
+  			}
+    }
+}
+
+//function for polling the input for the loader, and acting on the loader.
+void pollToggles()
+{
+		//go forward with drum
+		if(mController.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+		{
+				//if the forward button toggle isn't on then continute
+				if(!buttonPressedF)
+				{
+						//actaully flip the toggle, this is why the type has to be int
+						buttonToggleF = 1 - buttonToggleF;
+						//changed button pressed to true
+						buttonPressedF = 1;
+						//change the backward toggle to false so we don't try to go backwards and forwards
+						buttonToggleR = false;
+				}
+		}
+		//switch back to normal buttton state but leave toggle on if button isn't pressed.
+		else
+			buttonPressedF = 0;
+
+	//if our forward toggle is on, then eat the balls :D
+	if(buttonToggleF == true)
+	{
+			LoadServ.move(90);
+		  LoadServ2.move(90);
+	}
+	//check if other toggle is on if we need to really stop the motor
+	else
+	{
+			if(!buttonToggleR && !buttonToggleF)
+			{
+					LoadServ.move(0);
+				  LoadServ2.move(0);
+			}
+	}
+	//go backwards with drum
+	if(mController.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+	{
+			//if we haven't pressed the button then toggle the button
+			if(!buttonPressedR)
+			{
+					//actually toggle the button, this is why the type is int
+					buttonToggleR = 1 - buttonToggleR;
+
+					buttonPressedR = 1;
+
+					//so we stop going forward.
+					buttonToggleF = false;
+			}
+	}
+	//else, then turn button pressed to false
+	else
+		buttonPressedR = 0;
+
+	//if backward button toggle is on, then start the motor backward
+	if(buttonToggleR == true)
+	{
+			LoadServ.move(-90);
+		  LoadServ2.move(-90);
+	}
+  	//else, check if the forward toggle is off, then stop.
+  	else
+  	{
+  			if(!buttonToggleF && !buttonToggleR)
+  			{
+  					LoadServ.move(0);
+  			  	LoadServ2.move(0);
+  			}
+  	}
+}
+
+bool rev = false;
+//the driving code
+void driveControl()
+{
+//infinite loop to keep the driving code going.
+ while(true)
+ {
+	 //the toggle for reversing the drive train
+	 if(mController.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+	 {
+			 if(!reversePressed)
+			 {
+					 reverseToggle = 1 - reverseToggle;
+
+					 reversePressed = 1;
+			 }
+	 }
+	 else
+		 reversePressed = 0;
+
+	 if(reverseToggle)
+	 {
+			 rev = true;
+	 }
+	 else
+	 {
+			 if(!reverseToggle)
+			 {
+					 rev = false;
+			 }
+	 }
+	 //end toggle code
+
+	 //if we are not reversed, then drive normally, else drive reversed.
+	 if(!rev)
+	 {
+		 	leftMotB.move(mController.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+			leftMotF.move(mController.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+
+			rightMotB.move(mController.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+		 	rightMotF.move(mController.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+	 }
+	 else
+	 {
+		 leftMotB.move(-mController.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+		 leftMotF.move(-mController.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+
+		 rightMotB.move(-mController.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+		 rightMotF.move(-mController.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+	 }
+
+	 //poll the shooter and loader
+	 pollTFly();
+	 pollToggles();
+ }
+}
+
+//operator control entry point
+void opcontrol()
+{
+	//actually set the signature from the data we collected from the vision utility.
+	//set the data to index of 1 to reference later.
+  driveControl();
+}
---
+++ src/autonomous.cpp
@@ -0,0 +1,526 @@
+#include "main.h"
+//for std::abs
+#include<cstdlib>
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+
+
+using namespace okapi;
+
+//for checking the error state of errno fo debugging.
+extern int errno;
+
+//the actaull signature data from the vision sensor utility.
+pros::vision_signature_s_t B_FLAG = pros::Vision::signature_from_utility(2, -3121, 85, -1518, 2743, 12577, 7660, 1.2, 0);
+pros::vision_signature_s_t R_FLAG;
+pros::vision_signature_s_t GR = pros::Vision::signature_from_utility(1, -2591, -1797, -2194, -5113, -3723, -4418, 3, 0);
+
+pros::ADIUltrasonic frontR (F_R_O, F_R_Y);
+pros::ADIUltrasonic frontL(F_L_O, F_L_Y);
+//a function that alligns with the green part of the flag using the vision sensor, finds the largest green object on flags.
+static void testV()
+{
+	//basically resetting the vision sensor.
+	vSensor.clear_led();
+	//setting all of the signatures to an index that can be referenced later.
+	vSensor.set_signature(1, &GR);
+	vSensor.set_signature(2, &B_FLAG);
+	vSensor.set_signature(3, &R_FLAG);
+
+	//infinate loop so we can update the position of the vision object we find, for allignment.
+	while(true)
+	{
+		//get the largest object(0), from the first signature of the vision sensor.
+		//we call this every update to get the new position of the object
+		pros::vision_object_s_t rtn = vSensor.get_by_sig(0, 1);
+		std::cout<<rtn.x_middle_coord<<std::endl;
+	}
+}
+static void readVB()
+{
+  //basically resetting the vision sensor.
+	vSensor.clear_led();
+  //setting all of the signatures to an index that can be referenced later.
+  vSensor.set_signature(1, &GR);
+  vSensor.set_signature(2, &B_FLAG);
+  vSensor.set_signature(3, &R_FLAG);
+
+  //infinate loop so we can update the position of the vision object we find, for allignment.
+	while(true)
+	{
+		//get the largest object(0), from the first signature of the vision sensor.
+		//we call this every update to get the new position of the object
+		pros::vision_object_s_t rtn = vSensor.get_by_sig(0, 1);
+
+		//if it is within range, stop the motors.
+		if(!(rtn.x_middle_coord > 10) && !(rtn.x_middle_coord < 0))
+		{
+			rightMotB.move(0);
+			leftMotB.move(0);
+			break;
+		}
+		//if the object is to the left, then turn the robot left.
+		if(rtn.x_middle_coord < -5)
+		{
+			rightMotB.move(50);
+			leftMotB.move(-50);
+		}
+		else
+		{
+			//if the object is to the right, then turn the robot right.
+			if(rtn.x_middle_coord > 5)
+			{
+				rightMotB.move(-40);
+				leftMotB.move(40);
+			}
+		}
+		//so we don't starv other tasks like updating the LCD
+		pros::Task::delay(10);
+	}
+}
+static void readV()
+{
+  //basically resetting the vision sensor.
+	vSensor.clear_led();
+  //setting all of the signatures to an index that can be referenced later.
+  vSensor.set_signature(1, &GR);
+  vSensor.set_signature(2, &B_FLAG);
+  vSensor.set_signature(3, &R_FLAG);
+
+  //infinate loop so we can update the position of the vision object we find, for allignment.
+	while(true)
+	{
+		//get the largest object(0), from the first signature of the vision sensor.
+		//we call this every update to get the new position of the object
+		pros::vision_object_s_t rtn = vSensor.get_by_sig(0, 1);
+
+		//if it is within range, stop the motors.
+		if(!(rtn.x_middle_coord < -20) && !(rtn.x_middle_coord > -10))
+		{
+			rightMotB.move(0);
+			leftMotB.move(0);
+
+			rightMotF.move(0);
+			leftMotF.move(0);
+			break;
+		}
+		//if the object is to the left, then turn the robot left.
+		if(rtn.x_middle_coord < -5)
+		{
+			rightMotB.move(50);
+			leftMotB.move(-50);
+
+			rightMotF.move(50);
+			leftMotF.move(-50);
+		}
+		else
+		{
+			//if the object is to the right, then turn the robot right.
+			if(rtn.x_middle_coord > 5)
+			{
+				rightMotB.move(-40);
+				leftMotB.move(40);
+
+				rightMotF.move(-40);
+				leftMotF.move(40);
+			}
+		}
+		//so we don't starv other tasks like updating the LCD
+		pros::Task::delay(10);
+	}
+}
+static int getDif()
+{
+	int ret = 0;
+
+	pros::Task::delay(50);
+	ret += frontR.get_value();
+	pros::Task::delay(50);
+	ret -= frontL.get_value();
+
+
+	return std::abs(ret);
+
+}
+
+static int getLeft()
+{
+	pros::Task::delay(50);
+	return frontL.get_value();
+}
+
+static int getRight()
+{
+	pros::Task::delay(50);
+	return frontR.get_value();
+}
+//a test function for using the ultrasonic sensor to line the robot up without hitting a wall.
+static void allignFront(int dis)
+{
+  while(true)
+  {
+    if(getDif() < 50)
+    {
+        break;
+    }
+    else
+    {
+      if(getRight() > getLeft())
+      {
+        rightMotB.move(50);
+        leftMotB.move(-50);
+      }
+      else
+      {
+        rightMotB.move(-50);
+        leftMotB.move(50);
+      }
+    }
+    pros::Task::delay(10);
+  }
+  leftMotB.move(0);
+  leftMotF.move(0);
+
+  rightMotB.move(0);
+  rightMotF.move(0);
+
+
+	int temp;
+  while(std::abs(temp - dis) > 100)
+  {
+    temp = ((getRight() + getLeft()) / 2);
+
+    if(temp > dis)
+    {
+      leftMotB.move(40);
+      leftMotF.move(40);
+
+      rightMotB.move(40);
+      rightMotF.move(40);
+    }
+    else
+    {
+      leftMotB.move(-40);
+      leftMotF.move(-40);
+
+      rightMotB.move(-40);
+      rightMotF.move(-40);
+    }
+    pros::Task::delay(10);
+  }
+  leftMotB.move(0);
+  leftMotF.move(0);
+
+  rightMotB.move(0);
+  rightMotF.move(0);
+}
+
+//load forever until stopped with stopLoader() or manually.
+ static void loadf()
+ {
+   LoadServ.move(127);
+   LoadServ2.move(127);
+ }
+
+ //load forever reversed, same rules as loadf()
+ static void loadfr()
+ {
+   LoadServ.move(-127);
+   LoadServ2.move(-127);
+ }
+ //load normally for a set ammount of miliseconds, then stop the loader.
+ static void load(std::uint32_t mili)
+ {
+   LoadServ.move(127);
+   LoadServ2.move(127);
+   pros::Task::delay(mili);
+   LoadServ.move(0);
+   LoadServ2.move(0);
+ }
+ //load reversed for a set ammount of miliseconds, then stop the loader.
+ static void loadR(std::uint32_t mili)
+ {
+   LoadServ.move(-127);
+   LoadServ2.move(-127);
+   pros::Task::delay(mili);
+   LoadServ.move(0);
+   LoadServ2.move(0);
+ }
+ //stop the loading system.
+ static void stopLoader()
+ {
+   LoadServ.move(0);
+   LoadServ2.move(0);
+ }
+ //stop the fly wheel
+  static void stopShooter()
+  {
+    fly.move(0);
+    fly2.move(0);
+  }
+  //turn on the shooter for a set amount of seconds.
+ static void shoot(std::uint32_t mili)
+ {
+   fly.move_velocity(530);
+   fly2.move_velocity(530);
+   pros::Task::delay(mili);
+ }
+//the robot chassis
+ static auto Chassis = ChassisControllerFactory::create
+ (
+   //left drive train
+   {1, 2},
+   //right drive train, reversed hence the -
+   {-11, -12},
+   //the motors are using the green gearset.
+   AbstractMotor::gearset::green,
+   //the wheel diameter is 4 in, the chassis from middle of wheel to middle of wheel horizontally is 14.5 in
+   {4_in, 14.5_in}
+ );
+//front of second tick, far from flag.
+static void farRed()
+{
+  Chassis.moveDistance(1_ft);
+  Chassis.turnAngle(10_deg);
+  shoot(2500);
+  loadf();
+  pros::Task::delay(1500);
+  Chassis.moveDistance(-1_ft);
+  Chassis.turnAngle(70_deg);
+  Chassis.moveDistance(40_in);
+  Chassis.moveDistance(-4_in);
+  Chassis.turnAngle(97_deg);
+  Chassis.moveDistance(-3_ft);
+  /*
+  Chassis.setMaxVelocity(100);
+  Chassis.moveDistance(24_in);
+  Chassis.turnAngle(90_deg);
+  Chassis.setMaxVelocity(200);
+  Chassis.moveDistance(3.5_ft);
+  */
+}
+
+
+static void ALT_BLUE_C()
+{
+	//shoot top top left flag
+	shoot(1500);
+	loadf();
+	pros::Task::delay(200);
+	stopLoader();
+
+	//slow down the chassis to get more accurate turns.
+	Chassis.setMaxVelocity(50);
+	//realign after shooting from an angle
+	Chassis.turnAngle(7_deg);
+	//speed up the chassis again, we only have a minute after all.
+	Chassis.setMaxVelocity(160);
+	//toggle the bottom left flag after shooting.
+	Chassis.moveDistance(47_in);
+
+	//get out of flag and turn towards cap(first ball)
+	Chassis.moveDistance(-44_in);
+	Chassis.turnAngle(-102_deg);
+	Chassis.moveDistance(-10_in);
+	//turn on the loader to get the ball and drive for it.
+	loadf();
+	Chassis.moveDistance(44_in);
+	//give time to load
+	stopLoader();
+
+	//turn towards flag and move back a bit to get base allignment.
+	Chassis.turnAngle(87_deg);
+//  Chassis.moveDistance(-2_in);
+	Chassis.moveDistance(3_ft);
+	//allign with the biggest green object on flag on the x coord.
+	readVB();
+
+	//reverse to make sure we didn't load to much and the ball will get caught in the fly wheel
+	loadR(100);
+	//shoot and stop the loader and shooter
+	load(1000);
+	stopLoader();
+	stopShooter();
+
+		Chassis.turnAngle(10_deg);
+
+		Chassis.setMaxVelocity(200);
+	//hit the bottom middle flag
+	Chassis.moveDistance(28_in);
+}
+
+
+//start at red and move to gray, move forward to back of next tick
+static void ALT_RED_C()
+{
+	//shoot top top left flag
+	shoot(1500);
+	loadf();
+	pros::Task::delay(200);
+	stopLoader();
+
+	//slow down the chassis to get more accurate turns.
+	Chassis.setMaxVelocity(50);
+	//realign after shooting from an angle
+	Chassis.turnAngle(-5_deg);
+	//speed up the chassis again, we only have a minute after all.
+	Chassis.setMaxVelocity(160);
+	//toggle the bottom left flag after shooting.
+	Chassis.moveDistance(47_in);
+
+	//get out of flag and turn towards cap(first ball)
+	Chassis.moveDistance(-46_in);
+	Chassis.turnAngle(102_deg);
+	Chassis.moveDistance(-11_in);
+
+	//turn on the loader to get the ball and drive for it.
+	loadf();
+	Chassis.moveDistance(43_in);
+
+	stopLoader();
+
+	//turn towards flag and move back a bit to get base allignment.
+	Chassis.turnAngle(-76_deg);
+//  Chassis.moveDistance(-2_in);
+	Chassis.moveDistance(3_ft);
+	//allign with the biggest green object on flag on the x coord.
+	//readV();
+
+	//shoot and stop the loader and shooter
+	load(1000);
+	stopLoader();
+	stopShooter();
+
+		Chassis.turnAngle(-15_deg);
+
+		Chassis.setMaxVelocity(200);
+	//hit the bottom middle flag
+	Chassis.moveDistance(28_in);
+}
+
+static void farBlue()
+{
+  shoot(2500);
+  loadf();
+  pros::Task::delay(2000);
+  /*
+  Chassis.setMaxVelocity(100);
+  Chassis.moveDistance(24_in);
+  Chassis.turnAngle(-90_deg);
+  Chassis.setMaxVelocity(200);
+  Chassis.moveDistance(3.5_ft);
+  */
+}
+//start front of first tick, farthest from flag
+
+
+
+
+//back of second tick
+static void skillz()
+{
+  //shoot top top left flag
+  shoot(2000);
+  loadf();
+  pros::Task::delay(1250);
+  stopShooter();
+  stopLoader();
+
+  //slow down the chassis to get more accurate turns.
+  Chassis.setMaxVelocity(50);
+  //realign after shooting from an angle
+  Chassis.turnAngle(-10);
+  //speed up the chassis again, we only have a minute after all.
+  Chassis.setMaxVelocity(125);
+  //toggle the bottom left flag after shooting.
+  Chassis.moveDistance(48_in);
+
+  //get out of flag and turn towards cap(first ball)
+  Chassis.moveDistance(-44_in);
+  Chassis.turnAngle(102_deg);
+  //turn on the loader to get the ball and drive for it.
+  loadf();
+  Chassis.moveDistance(38_in);
+  //give time to load
+  pros::Task::delay(100);
+  stopLoader();
+
+  //turn towards flag and move back a bit to get base allignment.
+  Chassis.turnAngle(-79_deg);
+  Chassis.moveDistance(-2_in);
+  //allign with the biggest green object on flag on the x coord.
+  readV();
+
+  //reverse to make sure we didn't load to much and the ball will get caught in the fly wheel
+  loadR(100);
+  //shoot and stop the loader and shooter
+  shoot(2000);
+  load(1400);
+  stopLoader();
+  stopShooter();
+
+  //hit the bottom middle flag
+  Chassis.moveDistance(28_in);
+  Chassis.turnAngle(-17_deg);
+  Chassis.setMaxVelocity(140);
+  Chassis.moveDistance(24_in);
+  Chassis.setMaxVelocity(125);
+
+  //get out of flags
+  Chassis.moveDistance(-24_in);
+
+  //turn towards cap to flip and turn on the loader reversed so the robot doesn't eat the cap, then go for it.
+  Chassis.turnAngle(-97_deg);
+  loadfr();
+  Chassis.moveDistance(21_in);
+
+  Chassis.turnAngle(195_deg);
+  Chassis.moveDistance(62_in);
+  Chassis.moveDistance(-3_in);
+
+  Chassis.turnAngle(97_deg);
+  Chassis.moveDistance(20_in);
+  Chassis.turnAngle(95_deg);
+  loadf();
+  Chassis.moveDistance(12_in);
+  pros::Task::delay(100);
+  Chassis.turnAngle(148_deg);
+  loadR(100);
+  readV();
+  shoot(2000);
+  load(1400);
+
+  stopShooter();
+  Chassis.moveDistance(24_in);
+  Chassis.turnAngle(-12);
+  Chassis.moveDistance(24_in);
+  Chassis.turnAngle(-15_deg);
+  Chassis.moveDistance(8_in);
+
+  Chassis.moveDistance(-24_in);
+  Chassis.turnAngle(-97_deg);
+  Chassis.moveDistance(78_in);
+  Chassis.turnAngle(-97_deg);
+  Chassis.moveDistance(44_in);
+  Chassis.turnAngle(100_deg);
+  Chassis.setMaxVelocity(200);
+  Chassis.moveDistance(-6_ft);
+}
+void autonomous()
+{
+	while(true)
+	{
+		std::cout<<"LEft : "<<getLeft()<<std::endl;
+		std::cout<<"Right : "<<getRight()<<std::endl;
+	}
+	ALT_RED_C();
+}
