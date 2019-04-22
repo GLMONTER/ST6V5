228c1d019c84d12beab38511144a65c7c0203a49
Tad Keller <logischgl@gmail.com>
2019-04-22 14:56:36 -0500

    removed fat ass vscode shit ass microsoft files

--- .vscode/cquery_cached_index/c@@users@logis@documents@st6v5/include@sigs.hpp
+++
@@ -1,3 +0,0 @@
-#include"main.h"
-extern pros::vision_signature_s_t B_FLAG;
-extern pros::vision_signature_s_t R_FLAG;
--- .vscode/cquery_cached_index/c@@users@logis@documents@st6v5/src@sigs.cpp
+++
@@ -1,3 +0,0 @@
-#include"sigs.hpp"
-extern pros::vision_signature_s_t B_FLAG = pros::Vision::signature_from_utility(2, -3469, -2519, -2994, 11367, 14763, 13064, 5.9, 0);
-extern pros::vision_signature_s_t R_FLAG = pros::Vision::signature_from_utility(1, 7505, 11963, 9734, -541, 1, -270, 3.5, 0);
